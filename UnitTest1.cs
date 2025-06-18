70417decf2abe6dbf894ee3e8603b56b8d5ad1f6
Soomin Song <d.song.149@gmail.com>
2025-06-18 13:17:32 -0700

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿namespace TestLeetCode169
+{
+    public class Tests
+    {
+        LeetCode169.Solution Solution { get; set; }
+        int[] Input { get; set; }
+        int ExpectedOutput { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Solution = new();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Input = [3, 2, 3];
+            ExpectedOutput = 3;
+            Assert.That(Solution.MajorityElement(Input), Is.EqualTo(ExpectedOutput));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Input = [2, 2, 1, 1, 1, 2, 2];
+            ExpectedOutput = 2;
+            Assert.That(Solution.MajorityElement(Input), Is.EqualTo(ExpectedOutput));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Input = [7];
+            ExpectedOutput = 7;
+            Assert.That(Solution.MajorityElement(Input), Is.EqualTo(ExpectedOutput));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Input = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5];
+            ExpectedOutput = 5;
+            Assert.That(Solution.MajorityElement(Input), Is.EqualTo(ExpectedOutput));
+        }
+    }
+}
